6fae48a479b2409f856e9d0e410ca690e1172f81
Vidisha Mishra <vidishamishra133@gmail.com>
2022-09-10 19:32:43 +0530

    app change

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import time
 import pymongo
 import base64
 import requests
-#import pandas as pd
 from selenium import webdriver
 import mysql.connector as conn
 from flask import Flask, render_template, request,jsonify
